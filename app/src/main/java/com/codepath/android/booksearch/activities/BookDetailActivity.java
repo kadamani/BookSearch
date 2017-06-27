96f966a4a8d411387a56d545b8fe4166cf8490de
Samer Kadamani <samerk@fb.com>
2017-06-27 13:41:41 -0700

    Initial (maybe final) commit

---
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
@@ -0,0 +1,154 @@
+package com.codepath.android.booksearch.activities;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.GlideBitmapDrawable;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.codepath.android.booksearch.R;
+import com.codepath.android.booksearch.models.Book;
+
+import org.parceler.Parcels;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BookDetailActivity extends AppCompatActivity {
+    private ImageView ivBookCover;
+    private TextView tvTitle;
+    private TextView tvAuthor;
+    private ShareActionProvider miShareAction;
+    private Intent shareIntent;
+
+    Book book;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_detail);
+        // Fetch views
+        ivBookCover = (ImageView) findViewById(R.id.ivBookCover);
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvAuthor = (TextView) findViewById(R.id.tvAuthor);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Extract book object from intent extras
+        book = (Book) Parcels.unwrap(getIntent().getParcelableExtra(Book.class.getName()));
+        tvTitle.setText(book.getTitle());
+        tvAuthor.setText(book.getAuthor());
+        Glide.with(this)
+                .load(book.getCoverUrl())
+                .placeholder(R.drawable.ic_nocover)
+                .into(ivBookCover);
+
+        // Use book object to populate data into views
+        ImageView ivImage = (ImageView) findViewById(R.id.ivBookCover);
+        // Load image async from remote URL, setup share when completed
+        Glide.with(this).load(book.getCoverUrl())
+                .listener(new RequestListener<String, GlideDrawable>() {
+                              @Override
+                              public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                                  return false;
+                              }
+
+                              @Override
+                              public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                                  prepareShareIntent(((GlideBitmapDrawable) resource).getBitmap());
+                                  attachShareIntentAction();
+                                  // Let Glide handle resource load
+                                  return false;
+                              }
+                          }
+                )
+                .into(ivImage);
+    }
+
+    public Uri getBitmapFromDrawable(Bitmap bmp){
+
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            // Use methods on Context to access package-specific directories on external storage.
+            // This way, you don't need to request external read/write permission.
+            // See https://youtu.be/5xVh-7ywKpE?t=25m25s
+            File file =  new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+
+            // wrap File object into a content provider. NOTE: authority here should match authority in manifest declaration
+            bmpUri = FileProvider.getUriForFile(BookDetailActivity.this, "com.codepath.fileprovider", file);  // use this version for API >= 24
+
+            // **Note:** For API < 24, you may use bmpUri = Uri.fromFile(file);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
+
+
+    // Gets the image URI and setup the associated share intent to hook into the provider
+    public void prepareShareIntent(Bitmap drawableImage) {
+        // Fetch Bitmap Uri locally
+        Uri bmpUri = getBitmapFromDrawable(drawableImage);// see previous remote images section and notes for API > 23
+        // Construct share intent as described above based on bitmap
+        shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        shareIntent.setType("image/*");
+    }
+
+    // Attaches the share intent to the share menu item provider
+    public void attachShareIntentAction() {
+        if (miShareAction != null && shareIntent != null)
+            miShareAction.setShareIntent(shareIntent);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_book_detail, menu);
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        // Fetch reference to the share action provider
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        // Return true to display menu
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/codepath/android/booksearch/adapters/BookAdapter.java
@@ -0,0 +1,109 @@
+package com.codepath.android.booksearch.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.android.booksearch.R;
+import com.codepath.android.booksearch.activities.BookDetailActivity;
+import com.codepath.android.booksearch.models.Book;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
+    private List<Book> mBooks;
+    private Context mContext;
+
+    // Usually involves inflating a layout from XML and returning the holder
+    @Override
+    public BookAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        mContext = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+
+        // Inflate the custom layout
+        View bookView = inflater.inflate(R.layout.item_book, parent, false);
+
+        // Return a new holder instance
+        BookAdapter.ViewHolder viewHolder = new BookAdapter.ViewHolder(bookView);
+        return viewHolder;
+    }
+    // View lookup cache
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener  {
+        public ImageView ivCover;
+        public TextView tvTitle;
+        public TextView tvAuthor;
+
+        public ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+
+            ivCover = (ImageView)itemView.findViewById(R.id.ivBookCover);
+            tvTitle = (TextView)itemView.findViewById(R.id.tvTitle);
+            tvAuthor = (TextView)itemView.findViewById(R.id.tvAuthor);
+
+            itemView.setOnClickListener(this);
+        }
+
+        // handles row being clicked
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition(); // gets item position
+            if (position != RecyclerView.NO_POSITION) { // Check if an item was deleted, but the user clicked it before the UI removed it
+                // perform task
+                Intent i = new Intent(mContext, BookDetailActivity.class);
+                i.putExtra(Book.class.getName(), Parcels.wrap(mBooks.get(position)));
+                // We can access the data within the views
+                mContext.startActivity(i);
+
+
+            }
+        }
+    }
+
+    public BookAdapter(Context context, ArrayList<Book> aBooks) {
+        mBooks = aBooks;
+        mContext = context;
+    }
+
+
+
+
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(BookAdapter.ViewHolder viewHolder, int position) {
+        // Get the data model based on position
+        Book book = mBooks.get(position);
+
+        // Populate data into the template view using the data object
+        viewHolder.tvTitle.setText(book.getTitle());
+        viewHolder.tvAuthor.setText(book.getAuthor());
+        Glide.with(getContext())
+                .load(Uri.parse(book.getCoverUrl()))
+                .placeholder(R.drawable.ic_nocover)
+                .into(viewHolder.ivCover);
+        // Return the completed view to render on screen
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return mBooks.size();
+    }
+
+    // Easy access to the context object in the recyclerview
+    private Context getContext() {
+        return mContext;
+    }
+}
